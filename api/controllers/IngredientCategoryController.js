c1143edcf76906adff57a3c124edd62db7c80ca0
Duc Bui <buihongduc132@yahoo.com>
2015-02-04 15:41:05 +0700

    add create, read in ingredientcategory/manage/view

--- api/controllers/IngredientCategoryController.js
+++ api/controllers/IngredientCategoryController.js
@@ -6,6 +6,10 @@
  */
 
 module.exports = {
-	
+    viewManage: function(req, res) {
+        IngredientCategory.find().exec(function (err, ingredients) {
+            return res.view('ingredient_category_manage_view', {data: ingredients}); 
+        });
+    },
 };
 
--- config/routes.js
+++ config/routes.js
@@ -101,4 +101,5 @@ module.exports.routes = {
   '/services/model': 'ServicesController.getModel',
 
   '/product/manage/view': 'ProductController.viewManage',
+  '/ingredientcategory/manage/view': 'IngredientCategoryController.viewManage',
 };
