37dfe2ad2ebe1764a3688c825949193e77074419
Huy Tran <watsu999@zing.vn>
2015-02-09 11:25:51 +0700

    Complete 90%-Waiting for Export API

--- config/routes.js
+++ config/routes.js
@@ -69,6 +69,9 @@ module.exports.routes = {
     controller: 'IngredientController',
     action: 'view'
   },
+  'get /export': {
+    view: 'view_export_ingredient'
+  },
   'get /warninglimit':{
     view: 'view_warninglimit',
     locals:{
